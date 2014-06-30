eae70235a082a651f82874a30126abb01f78d30c
hernan <hquintana@devosinc.com>
2014-06-30 00:45:32 -0500

    Commit inicial del proyecto

---
+++ src/ulima/isw2/loginapp/LoginServlet.java
@@ -0,0 +1,51 @@
+package ulima.isw2.loginapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null)
+				jb.append(line);
+		} catch (Exception e) { /*report an error*/ }
+		String resp = jb.toString();
+		System.out.println("Datos: " + resp);
+		JSONObject js = (JSONObject) JSONValue.parse(resp);
+		String usuario = js.get("usuario").toString();
+		System.out.println("Usuario: " + usuario);
+		response.getOutputStream().write("OK".getBytes());
+	}
+
+}
